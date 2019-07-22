9f3f64b41e6d76f09227e681a3fd8137344d82bd
kcodek <career_kk@yahoo.com>
2019-07-22 14:16:27 +0530

    Add Creational Patterns for SchmidH

--- 1.CREATIONAL/Singleton/singleton_vinceh/singleton_after.cpp
+++ 1.CREATIONAL/Singleton/singleton_vinceh/singleton_after.cpp
@@ -5,51 +5,59 @@ using std::endl;
 
 // New design.  "globalObj" is now a private static data member of its own class.  Global access is provided by the public static member
 // function inst().  And the lazy initialization code is encapsulated in the inst() function.
-// GlobalClass's ctor and dtor have been made protected so that clients cannot create more inst's or destroy the Singleton inst. 
-class GlobalClass {
+// GlobalClass's ctor and dtor have been made protected so that clients cannot create more inst's or destroy the Singleton inst.
+class GlobalClass
+{
 public:
-   int  getValue() {
-      return value_; }
-   void setValue( int v ) {
-      value_ = v; }
-   static GlobalClass* inst() {
-      if ( ! globalObj_ )
+   int getValue()
+   {
+      return value_;
+   }
+   void setValue(int v)
+   {
+      value_ = v;
+   }
+   static GlobalClass *inst()
+   {
+      if (!globalObj_)
          globalObj_ = new GlobalClass;
-      return globalObj_; }
+      return globalObj_;
+   }
+
 protected:
-   GlobalClass( int v=0 ) {
-      value_ = v; }
-   ~GlobalClass() { }
+   GlobalClass(int v = 0)
+   {
+      value_ = v;
+   }
+   ~GlobalClass() {}
+
 private:
-   int    value_;
-   static GlobalClass* globalObj_;
+   int value_;
+   static GlobalClass *globalObj_;
 };
 
 // Allocating and initializing GlobalClass's static data member (the ptr, not a GlobalClass inst)
-GlobalClass*
-   GlobalClass::globalObj_ = 0;
+GlobalClass *
+    GlobalClass::globalObj_ = 0;
 
-void foo( void )
+void foo(void)
 {
-   GlobalClass::inst()->setValue( 1 );
-   cout << "foo: globalObj is " <<
-      GlobalClass::inst()->getValue()
-      << endl;
+   GlobalClass::inst()->setValue(1);
+   cout << "foo: globalObj is " << GlobalClass::inst()->getValue()
+        << endl;
 }
 
-void bar( void )
+void bar(void)
 {
-   GlobalClass::inst()->setValue( 2 );
-   cout << "bar: globalObj is " <<
-      GlobalClass::inst()->getValue()
-      << endl;
+   GlobalClass::inst()->setValue(2);
+   cout << "bar: globalObj is " << GlobalClass::inst()->getValue()
+        << endl;
 }
 
-int main( void )
+int main(void)
 {
-   cout << "main: globalObj is " <<
-      GlobalClass::inst()->getValue()
-      << endl;
+   cout << "main: globalObj is " << GlobalClass::inst()->getValue()
+        << endl;
    foo();
    bar();
    return 0;
